8703ac5a43e7b09cc2cd490d14d848e4e61df574
znatz <z.nat.cn@gmail.com>
2012-10-10 02:10:48 +0900

    Position of line in Response header is calculated
    Need to work more on responseheader class

--- ResponseHeader.cpp
+++ ResponseHeader.cpp
@@ -4,6 +4,18 @@
 
 ResponseHeader::ResponseHeader(char* val) {
 FstBuff = val;
-HdrLen = UnicodeString(FstBuff).Pos(UnicodeString("\r\n\r\n"))+ 3;
+UnicodeString FullStr = UnicodeString(val);
+HdrLen = FullStr.Pos(UnicodeString("\r\n\r\n"))+ 3;
 
+	for (int i = 0; i < 20; i++) {
+Pos[i]=0;
+	}
+
+int i = 0;
+do
+{
+	Pos[i] = FullStr.Pos(UnicodeString("\r\n"))+2;
+	FullStr = FullStr.SubString(Pos[i],HdrLen-3);
+	i++;
+}while( i<20 && Pos[i-1]>=3 );
 }
